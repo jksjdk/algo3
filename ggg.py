cd7ee15a22dd1120aa4389d7964f6d1b8d9027ca
JOHAN STEVEN CARTAGENA GONZALEZ <j.steven.cartagonzalez@gmail.com>
2022-07-12 16:11:21 -0500

    queso

---
+++ ggg.py
@@ -0,0 +1,16 @@
+print ("hola mundo")
+print ("                 ..........")
+print ("              ,:;=*CYI/+++\IL=+,")
+print ("          .;&AZOYJI**+<;:-,,-:+=+L;,")
+print ("       ,AHRKOPJ**<---,,,...,,--:\>+IL\,")
+print ("     ,6EHKRZFJ*</---,,,.....,,--:;\>*IO\,")
+print ("    ,AEHRAZOVI+<<--,,,..........,,--;=*II\ ")
+print ("   ;AWHRKKXCI=/--,,,.............,,--;=*IL\ ")
+
+
+
+
+
+
+
+
